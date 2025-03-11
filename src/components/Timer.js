6463cfc4d04a8ee0c17be9243c8eaf1681a7fd46
ldebarba <ldebarba@ohgiftcard.com>
2025-03-11 10:37:34 -0300

    cambiando a mp3

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -14,8 +14,8 @@ function Timer() {
     const finishRef = useRef(null);
 
     useEffect(() => {
-        beepRef.current = new Audio(`${process.env.PUBLIC_URL}/beep.wav`);
-        finishRef.current = new Audio(`${process.env.PUBLIC_URL}/finish.wav`);
+        beepRef.current = new Audio(`${process.env.PUBLIC_URL}/beep.mp3`);
+        finishRef.current = new Audio(`${process.env.PUBLIC_URL}/finish.mp3`);
         beepRef.current.load();
         finishRef.current.load();
     }, []);
